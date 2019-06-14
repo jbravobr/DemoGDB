ec388063380ef9d68ad40472dda46d8727c46e08
Rodrigo Amaro <jbravo.br@gmail.com>
2019-06-13 23:08:52 -0300

    First Commit

---
+++ demoapp/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Xamarin.Forms;
+
+namespace demoapp
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+
+#if DEBUG
+            HotReloader.Current.Run(this);
+#endif
+
+            MainPage = new MainPage();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ demoapp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace demoapp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        private void Login_Click(object sender, EventArgs eventArgs)
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void OnTapGestureRecognizerTapped(object sender, EventArgs args)
+        {
+
+        }
+    }
+}
